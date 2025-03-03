0a3d4506f7ec1b43ec75401dd31d0bc661b1deb8
Markus Henriksen <markus.henriksen@cgi.com>
2025-03-03 19:57:34 +0100

    not use straming and add dolphin3

--- src/lib/ollama/helper/index.ts
+++ src/lib/ollama/helper/index.ts
@@ -8,17 +8,21 @@ export enum Model {
   DEEPSEEK_CODER = 'deepseek-coder:6.7b',
   DOLPHIN2_2_MISTRAL = 'dolphin2.2-mistral:latest',
   MEDLLAMA2 = 'medllama2:latest',
-  LLAMA_3B_3_2 = 'llama3.2:latest'
+  LLAMA_3B_3_2 = 'llama3.2:latest',
+  DOLPHIN3 = 'dolphin3'
 }
 
 const ollama = new ChatOllama({
-  model: Model.LLAMA_3B_3_2
+  model: Model.LLAMA_3B_3_2,
+  streaming: false,
+  checkOrPullModel: true
 })
 
 export const askOllama = async (question: string, model?: Model): Promise<string> => {
   if (model) {
     ollama.model = model
   }
+
   const answer = await ollama.invoke(question)
   return answer.content.toString()
 }
